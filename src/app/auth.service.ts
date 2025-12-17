84193ae38ac442729c81fb9119619cdb81ffb326
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2025-12-17 10:14:16 -0300

    Finalização dos Conceitos de Roteamentos

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -4,19 +4,23 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthService {
-
+  accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.KMUFsIDTnFmyG3nMiGM6H9FNFUROf3wh7SmqJp-QV30";
   constructor() { }
 
   estaAutenticado(): boolean{
-    localStorage
-    return true
+    return !!sessionStorage.getItem("access-token")
   }
 
-  login(email: string, senha: string){
+  login(email: string, senha: string): boolean{
+    if(email === "admin@admin.com.br" && senha === "123456"){
+      sessionStorage.setItem("access-token", this.accessToken)
+      return true;
+    }
 
+    return false;
   }
 
   logout(){
-
+    sessionStorage.clear();
   }
 }
